33183c87fd57ec3650403285030f0dbb4323a0da
Maria Eduarda Gonzaga <mariasilva.2024@alunos.utfpr.edu.br>
2026-05-06 13:30:01 -0300

    Update PE.c
    alterações com as bibliotecas para que elas ultilizem info ao invés de inteiros na struct nodo

--- PE.c
+++ PE.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include "PE.h"
 
-void push (Pilha *p, int x){
+//alterado para receber um info 
+void push (Pilha *p, info a){
     p -> topo ++;
-    p-> v[p-> topo] =x;
+    p-> v[p-> topo] = a;
 }
 
-int pop (Pilha *p){
-    int a  = p -> v[p->topo];
+//alterado para retornar um info
+info pop (Pilha *p){
+    info a  = p -> v[p->topo];
     p-> topo --;
     return a;
 }

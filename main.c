49962545fe89383f1a7e561f19e8e79451c286a8
Maria Eduarda Gonzaga <mariasilva.2024@alunos.utfpr.edu.br>
2026-05-15 15:01:41 -0300

    Update main.c
    adicionado as funções imprimi_mesa, e atacar

--- main.c
+++ main.c
@@ -6,6 +6,42 @@
 #include "PD.h"
 #include "info.h"
 
+void imprimi_mesa(Fila *f, Pilha *a, int *v, int b, info_t mao){
+    for (int i =0; i<b; i ++){
+        info_t temp = pop (&a[i]);
+        printf("%d: [%d %s] +%d \n", i++, temp.num, temp.cor, v[i]);
+        push(&a[i], temp);
+    }
+    printf("Na mao: [%d %s] \n Jogada:", mao.num, mao.cor);
+    return;
+}
+
+void atacar (Fila *f, Pilha *a, int *v, info_t mao, int numPilha){
+    int jogada;
+    scanf("%d", &jogada);
+
+    if (jogada <= numPilha){
+        if (v [jogada -1] != 0){
+            info_t x = pop(&a[jogada-1]);
+            if(x.cor == mao.cor || x.num == mao.num){
+            push(f,mao);
+            mao = pop(f);
+            push(f,mao);
+            v[jogada-1]=v[jogada-1]-1;
+            }else {
+                printf("Jogada invalida. \n Jogada:");
+                scanf("%d", &jogada);
+            }
+        }else {
+            printf("Jogada invalida. \n Jogada:");
+            scanf("%d", &jogada);
+        }
+    }else {
+        printf("Jogada invalida. \n Jogada:");
+        scanf("%d", &jogada);
+        }
+}
+
 int main (){
 
     //criação do baralho
@@ -29,15 +65,16 @@ int main (){
         baralho[i] = baralho [j];
         baralho[j]= temp;
     }
-
+    info_t mao = baralho[0];
     //lendo o numero de pilhas
     int numPilha;
     scanf("%d", &numPilha);
 
     //lendo e criando a fila
     int tamFila;
-    int idx =0;
+    int idx =1;
     scanf("%d", &tamFila);
+    int t = tamFila;
     Fila *f;
     if (tamFila > 0){
     f = cria_fila();
@@ -51,17 +88,24 @@ int main (){
     printf("a sua fila nao pode estar vazia");
 }
 
+
+tamFila = t;
 //criando as pilhas e colocando elas em um vetor
 Pilha *a[numPilha];
 for (int i =0; i<numPilha; i++){
     a[i]= criaPilha();
 }
-int b = (20-1-idx)/numPilha;
+int b = (20-1-tamFila)/numPilha;
+int v [numPilha];
+for (int i =0; i< numPilha; i++){
+    v[i] = b;
+}
 for (int i =0; i<numPilha; i++){
     for (int j =0; j<b; j++){
         push(a[i], baralho[idx]);
     }
 }
+
 
 
 }

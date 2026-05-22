6bbfcbf10b3101e4c290918886da5335001a8f0f
NandaBronel <gimenesmariafernanda002@gmail.com>
2026-05-22 12:10:35 -0300

    correção dos cométarios
    O primeiro comentário estsva sendo traduzido de maneira errada pelo sistema.

--- PE.h
+++ PE.h
@@ -2,7 +2,7 @@
 #define PE_H
 #include "info.h"
 #define MAX 100
-// Cria a estrutura que define a pilha;
+// Cria a estrutura que define a pilha (Create a structure to define the stack);
 typedef struct{
     info_t v[MAX];
     int topo;

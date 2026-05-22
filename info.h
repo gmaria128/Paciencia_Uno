6546674ad71c1dcd683c81c353f79fd9e88595b5
NandaBronel <gimenesmariafernanda002@gmail.com>
2026-05-22 11:38:20 -0300

    Adicionei comentários a info_h
    Added comments to clarify the purpose of the info_t structure.

--- info.h
+++ info.h
@@ -1,6 +1,6 @@
 #ifndef INFO_H
 #define INFO_H
-
+// Define o tipo info_t, que representa uma carta do baralho, designando um numero (0,1,2,3,4) e uma cor (amarelo, verde, azul ou vermelho);
 typedef struct{
     int num;
     char cor[10];
